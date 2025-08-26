e068925b86db5c61c8b2086b557d8eebb6ceb6ca
Aux-Developm3nt <endungamamama@gmail.com>
2025-08-27 00:32:46 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,25 @@
+-- Only supports Open Source Scripts from VantaXock
+local SUPPORTED_GAMES = {
+    [84316229168229] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Clone%20Chaos.lua",
+    [126849358937279] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Merge%20Gubby.lua", 
+    [135170308040342] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Grow%20Every%20Bite.lua",
+    [111452220770252] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Beat%20Up%20gubby%20on%20his%20own%20Home.lua",
+    [15407368046] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Trampoline%20Battle%20Simulator.lua",
+    [13675842775] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Get%20Big%20Simulator.lua",
+    [116495829188952] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Dead%20Rails.lua",
+    [14157644155] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Money%20Grab%20Simulator.lua",
+    [15506010437] = "https://raw.githubusercontent.com/Aux-Developm3nt/Open-Source-/main/Fun%20Squid%20game!%20Glass%20Bridge%202.lua",
+}
+
+local scriptURL = SUPPORTED_GAMES[game.PlaceId]
+
+if scriptURL then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(scriptURL))()
+    end)
+    if not success then
+        warn("Failed to load script:", err)
+    end
+else
+    warn("Game not supported. Are you sure you are on the right game?")
+end
